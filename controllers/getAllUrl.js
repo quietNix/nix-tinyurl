655bcaacbcbfe92514f45dc5f80be92235125cb0
Nikhil Kumar <42882230+quietNix@users.noreply.github.com>
2022-01-30 01:36:49 +0530

    Fine Tune

--- controllers/getAllUrl.js
+++ controllers/getAllUrl.js
@@ -2,7 +2,7 @@ const urls = require("../models/urls");
 
 const getAllUrl = async (req, res, next) => {
     try {
-        const allUrls = await urls.find({}).sort({created_at: -1}).limit(50);
+        const allUrls = await urls.find({}).sort({_id: -1}).limit(50);
         res.status(201).render("listUrls", {allUrls: allUrls})
     } catch (e) {
         if (e.code ==='ERR_HTTP_INVALID_STATUS_CODE')res.status(502).send("Server Not Responding")
